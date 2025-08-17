02e739e0d7d04bbae169432723045b3c34b57a34
Chanho Kim <ckim@tvo.org>
2025-08-17 11:39:49 -0400

    vue frontend init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
+
+const message = ref('Loading...')
+
+onMounted(async () => {
+  const res = await axios.get('http://localhost:8080/api/hello')
+  message.value = res.data
+})
+</script>
+
+<template>
+  <h1>{{ message }}</h1>
+</template>
